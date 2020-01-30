50e5e7e84b4f225345106edaa289a6dfa2c93d33
Lorenzo T. Tuppince <lorenzot.tuppince@Macs-MacBook-Pro.local>
2020-01-29 23:38:56 -0500

    2nd Commit

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -2,6 +2,8 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs")
+var db = require("../db/db.json")
 
 // Sets up the Express App
 // =============================================================
@@ -29,7 +31,32 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// API routes
+//=================================================
+app.get("/api/notes", function(req, res) {
+  return res.json(notes);
+});
 
+
+app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNotes = req.body;
+
+  console.log(newNotes);
+
+  db.push(newNotes);
+
+  fs.writeFile('./db/db.json', JSON.stringify(db), function(err) {
+    if (err) throw err;
+    console.log("newNotes")
+})
+
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+
+})
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
