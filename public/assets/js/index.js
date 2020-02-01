e78e9d803d0d53f85c70e01f6dcf01fa0559cb73
Lorenzo T. Tuppince <lorenzot.tuppince@Macs-MacBook-Pro.local>
2020-02-01 14:59:33 -0500

    This Worksgit add .git add .git add .git add .git add .

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -128,7 +128,8 @@ var renderNoteList = function(notes) {
 // Gets notes from the db and renders them to the sidebar
 var getAndRenderNotes = function() {
   return getNotes().then(function(data) {
-    renderNoteList(data);
+    console.log(data)
+    // renderNoteList(data);
   });
 };
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs")
-var db = require("../db/db.json")
+var db = require("./db/db.json")
 
 // Sets up the Express App
 // =============================================================
@@ -11,8 +11,9 @@ var app = express();
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 
@@ -22,19 +23,15 @@ app.use(express.json());
 // * GET `/notes` - Should return the `notes.html` file.
 app.get("/notes", function(req, res) {
     
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-//* GET `*` - Should return the `index.html` file
-app.get("*", function(req, res) {
-    
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
 // API routes
 //=================================================
 app.get("/api/notes", function(req, res) {
-  return res.json(notes);
+  // return res.json(notes);
+  res.send("This WORKS!!!!!!!")
 });
 
 
@@ -42,23 +39,30 @@ app.post("/api/notes", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   var newNotes = req.body;
-
+  
   console.log(newNotes);
-
+  
   db.push(newNotes);
-
+  
   fs.writeFile('./db/db.json', JSON.stringify(db), function(err) {
     if (err) throw err;
     console.log("newNotes")
-})
-
+  })
+  
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-
+  
 })
 // Starts the server to begin listening
 // =============================================================
+
+//* GET `*` - Should return the `index.html` file
+app.get("/", function(req, res) {
+  
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
